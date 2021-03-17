02886c4c48d488ef82ad1c965f2c6db0e4bcd8a9
Alex <AAArdyshev@yandex.ru>
2021-03-17 15:52:06 +0300

    объекты и массивы (ссылки)

--- js/script.js
+++ js/script.js
@@ -234,3 +234,207 @@ console.log(anotherNum); */
 /* console.log(parseInt(test)); переводит строку в число и округляет его */
 
 /* console.log(parseFloat(test)); переводит строку в число без округления дает так как есть */
+
+
+/* 
+---------------------------------------------------------------------Callback---------------------------------------------------------------------------------
+ */
+
+/* function first() {
+    // do something
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done () {
+    console.log('Im go to teacher');
+}
+
+learnJS('JavaScript', done); */
+
+
+/* ----------------------------------------------------------Object---------------------------------------------------------------------------------------- */
+
+/* const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors; */
+
+/* console.log(Object.keys(options).length);  */// пересчет свойств объекта
+
+/* console.log(options.name);
+
+delete options.name;
+
+console.log(options); */
+/* console.log(options["colors"] ["border"]);  */
+
+/* let counter = 0;
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+        
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+
+
+console.log(counter); */
+
+/* ------------------------------------------------------------------------ Massive----------------------------------------------------------------------------- */
+
+const arr = [5, 2, 3, 10, 8];
+
+arr.sort(compareNum); // call back a-b, тогда правильно сортирует числа
+
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+/* arr.pop(); */ // Удаляет последний элемент массива
+
+/* arr.push(10); */ // Добавляет в конец элемент массива
+
+/* for (let i = 0; i < arr.length; i++) { // length определяет сколько элементов в массиве 
+    console.log(arr[i]);
+} */
+
+/* for (let value of arr) {  // of переберает только масивоподобные 
+    console.log(value);
+}
+ */
+
+/* arr.forEach(function(item, i, arr) {    // Гибко перебирает массив, 1 арг - то, что будем перебирать элемент, 2- номер по порядку, 3 ссылка на массив break не работает тут
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+}); */
+
+const str = prompt("", "");
+const products = str.split(", "); // из строки формируем массив
+products.sort(); // Метод сортировки по алфавиту как строки
+console.log(products.join('; ')); // из массива формируем строку
+
+
+/* ------------------------------------Передача по ссылке---------------------------------------- */
+
+let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+
+/* const copy = obj; // Ccылка на существующий объект 
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj); */
+
+function copy (mainObj) {
+    let objCopy = {};
+
+    let key;
+    for(key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+console.log(Object.assign(numbers, add)); //метод создает поверхнотную копию и засовывает туда дополнительный (в намб засунули адд)
+
+const clone = Object.assign({}, add); // создает поверхнотную копию и засовывает туда дополнительный
+
+clone.d = 20;
+
+/* console.log(add);
+console.log(clone); */
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice(); // копирует массив метод слайс можно внутрь передать ограничения по кол-во
+
+
+newArray[1] = 'sadadadasdasdasd'
+console.log(newArray);
+console.log(oldArray);
+
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['worpdpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'fb']; // ... оператор разворачивает 1 арг 1 массив 2 арг второй массив, и добавили свое
+
+      console.log(internet);
+
+function log (a, b, c)  {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num =  [2, 5, 7];
+
+log(...num);
